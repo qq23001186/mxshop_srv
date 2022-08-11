11545e29f7f66fe85675c5d537df179a62a5a326
qq23001186 <qq23001186@163.com>
2022-08-11 17:34:34 +0800

    ver5.70 repair proto with goods and inventory

--- goods_srv/proto/goods.pb.go
+++ goods_srv/proto/goods.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.28.0
 // 	protoc        v3.21.1
-// source: order.proto
+// source: goods.proto
 
 package proto
 
--- inventory_srv/proto/inventory_grpc.pb.go
+++ inventory_srv/proto/inventory_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.21.2
-// source: order.proto
+// source: inventory.proto
 
 package proto
 
